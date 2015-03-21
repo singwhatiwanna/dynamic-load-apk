55c2f5ad953bde482abbc470ad7ebe944e15e687
zhangjie.zj <zhangjie.zj@alibaba-inc.com>
2015-03-21 09:49:01 +0800

    补充Service插件基类接口函数

--- DynamicLoadApk/lib/src/com/ryg/dynamicload/DLBasePluginService.java
+++ DynamicLoadApk/lib/src/com/ryg/dynamicload/DLBasePluginService.java
@@ -2,6 +2,7 @@ package com.ryg.dynamicload;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.IBinder;
 
 public class DLBasePluginService extends Service implements DLServicePlugin {
@@ -10,6 +11,66 @@ public class DLBasePluginService extends Service implements DLServicePlugin {
     public IBinder onBind(Intent intent) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void onDestroy() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onLowMemory() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        // TODO Auto-generated method stub
+        
     }
 
 }
